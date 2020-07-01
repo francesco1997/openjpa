055a5221159189b510bb54a1c77e3b04d29fd97a
Francesco Mancini <francesco140797@gmail.com>
2020-07-01 15:12:02 +0200

    Added PinCacheMapTest

---
+++ openjpa-kernel/src/test/java/org/apache/openjpa/util/PinCacheMapTest.java
@@ -0,0 +1,117 @@
+package org.apache.openjpa.util;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/*
+CacheMap maintains
+a fixed number of cache entries, and an
+optional soft reference map for entries
+that are moved out of the LRU space. So,
+for applications that have a monotonically
+increasing number of distinct queries, this
+option can be used to ensure that a fixed
+amount of memory is used by the cache.
+*/
+
+@RunWith(Parameterized.class)
+public class PinCacheMapTest {
+
+    private Object key;
+    private Object value;
+    private CacheMap cacheMap;
+    private boolean hasPreviousValue;
+    private boolean pinned;
+    private boolean expectedResult;
+
+
+    public PinCacheMapTest(TestInput testInput) {
+        this.key = testInput.getKey();
+        this.hasPreviousValue = testInput.isAlreadyExist();
+        this.pinned = testInput.isPinned();
+        this.expectedResult = testInput.isExpectedResult();
+        if (this.hasPreviousValue) {
+            this.value = testInput.getValue();
+        } else {
+            this.value = null;
+        }
+    }
+
+
+    @Parameterized.Parameters
+    public static Collection<TestInput> getParameters(){
+        List<TestInput> testInputs = new ArrayList<>();
+
+        testInputs.add(new TestInput(null, null, false,false, false));
+        testInputs.add(new TestInput(new Object(), new Object(), true,false, true));
+        testInputs.add(new TestInput(new Object(), null, false,true, false));
+        testInputs.add(new TestInput(new Object(), new Object(), true,true, true));
+
+        return testInputs;
+
+    }
+
+    private static class TestInput {
+        private Object key;
+        private Object value;
+        private boolean alreadyExist;
+        private boolean pinned;
+        private boolean expectedResult;
+
+        public TestInput(Object key, Object value, boolean alreadyExist, boolean pinned, boolean expectedResult) {
+            this.key = key;
+            this.value = value;
+            this.alreadyExist = alreadyExist;
+            this.pinned = pinned;
+            this.expectedResult = expectedResult;
+        }
+
+        public Object getKey() {
+            return key;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public boolean isAlreadyExist() {
+            return alreadyExist;
+        }
+
+        public boolean isPinned() {
+            return pinned;
+        }
+
+        public boolean isExpectedResult() {
+            return expectedResult;
+        }
+    }
+
+    @Before
+    public void setUp(){
+        this.cacheMap = new CacheMap(true);
+        if (this.hasPreviousValue) {
+            this.cacheMap.put(this.key, this.value);
+        }
+
+        if (this.pinned) {
+            this.cacheMap.pin(this.key);
+        }
+    }
+
+    @Test
+    public void pinTest() {
+        boolean result = this.cacheMap.pin(this.key);
+
+        Assert.assertEquals(this.expectedResult, result);
+
+    }
+
+}
